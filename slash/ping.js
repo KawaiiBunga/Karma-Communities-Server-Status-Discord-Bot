a25477a18fac61ad23070aa89834df39da10e893
KawaiiBunga <107073565+KawaiiBunga@users.noreply.github.com>
2023-01-18 03:02:52 -0600

    Create ping.js

---
+++ slash/ping.js
@@ -0,0 +1,24 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    name: 'ping',
+    description: '🏓Check my ping!',
+    run: async (client, interaction) => {
+      let pembed = new EmbedBuilder()
+		  .setColor('#2F3136')	
+		  .setTitle('Client Ping')
+		  .addFields({ name: '**Latency**', 
+                   value: `\`${Date.now() - interaction.createdTimestamp}ms\``
+                 })
+		  .addFields({ name: '**API Latency**', 
+                   value: `\`${Math.round(client.ws.ping)}ms\``
+                 })
+		  .setTimestamp()
+                  .setFooter({
+                     text: `${interaction.user.username}`,
+                     iconURL: interaction.user.displayAvatarURL()
+                  })
+                  
+      interaction.reply( { embeds: [pembed] } );
+    },
+};
