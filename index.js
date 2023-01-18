434a9b572e842fd1c00ecfdc207fad3f92a4b125
KawaiiBunga <107073565+KawaiiBunga@users.noreply.github.com>
2023-01-18 03:05:08 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,57 @@
-const { Client, Intents } = require('discord.js');
-const Gamedig = require('gamedig');
+const Discord = require("discord.js")
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
 
-const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+const client = new Client({
+  partials: [
+    Partials.Message, // for message
+    Partials.Channel, // for text channel
+    Partials.GuildMember, // for guild member
+    Partials.Reaction, // for message reaction
+  ],
+  intents: [
+    GatewayIntentBits.Guilds, // for guild related things
+    GatewayIntentBits.GuildInvites, // for guild invite managing
+    GatewayIntentBits.GuildMessages, // for guild messages things
+    GatewayIntentBits.GuildMessageReactions, // for message reactions things
+    GatewayIntentBits.MessageContent, // enable if you need message content things
+  ],
+});
+const fs = require("fs");
+const config = require("./config.json");
+client.config = config;
 
-client.on('ready', async () => {
-//CONFIG//////////////////////////////////////////////////////////////////////////////////////
-    const pingInterval = "5"; // How often the bot queries the server in seconds
-    const game = "garrysmod"; // https://www.npmjs.com/package/gamedig - Games List
-    const IP = "45.61.170.100"; // IP of server to query 
-    const port = "27015"; // Port of server to query   
-//CONFIG//////////////////////////////////////////////////////////////////////////////////////
+/* Load all events (discord based) */
 
-//NO TOUCH///////////////////////////////////////////////////////////////////////////////////
-    setInterval(async () => {
-        try {
-            const serverPing = await Gamedig.query({
-                type: game, 
-                host: IP, 
-                port: port 
-            });
-    
-            const playerCount = serverPing.players.length;
-            const maxPlayers = serverPing.maxplayers;
-//NO TOUCH////////////////////////////////////////////////////////////////////////////////////
+fs.readdir("./events/discord", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    const event = require(`./events/discord/${file}`);
+    let eventName = file.split(".")[0];
+    console.log(`[Event]   ✅  Loaded: ${eventName}`);
+    client.on(eventName, event.bind(null, client));
+    delete require.cache[require.resolve(`./events/discord/${file}`)];
+  });
+  console.log(`[Event] - ✅ Loaded all events!`);
+});
 
-//CHANGE STATUS///////////////////////////////////////////////////////////////////////////////
-            const status = `on KarmaRP - ${playerCount}/${maxPlayers}` // Bot status, "Playing on KarmaRP - x/x"
-//CHANGE STATUS///////////////////////////////////////////////////////////////////////////////
+// let interactions be a new collection ( slash commands  )
+client.interactions = new Discord.Collection();
+// creating an empty array for registering slash commands
+client.register_arr = []
+/* Load all slash commands */
+fs.readdir("./slash/", (_err, files) => {
+  files.forEach(file => {
+    if (!file.endsWith(".js")) return;
+    let props = require(`./slash/${file}`);
+    let commandName = file.split(".")[0];
+    client.interactions.set(commandName, {
+      name: commandName,
+      ...props
+    });
+    client.register_arr.push(props)
+    console.log(`[ / | Slash Command ] - ✅ Loaded: ${commandName}`);
+  });
+});
 
-//NO TOUCH///////////////////////////////////////////////////////////////////////////////////
-            client.user.setActivity(status)
-        }
-        catch (ex) {
-            console.log(ex);
-        }
-    }, pingInterval);
-
-    console.log("Bot initialized");
-})
-//NO TOUCH//////////////////////////////////////////////////////////////////////////////////
-
-const token = process.env.BOT_TOKEN;
-
-//BOT TOKEN/////////////////////////////////////////////////////////////////////////////////
-client.login("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"); // Change to discord bot token
-//BOT TOKEN/////////////////////////////////////////////////////////////////////////////////
+// Login through the client
+client.login(config.token);
